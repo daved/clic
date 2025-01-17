88be741f930147cf73f866e021bf325213dc4205
daved <daved@codemodus.com>
2025-01-17 15:38:49 -0800

    Expand pkg comment

--- clic.go
+++ clic.go
@@ -1,6 +1,22 @@
 // Package clic provides a structured multiplexer for CLI commands. In other
 // words, clic will parse CLI command arguments and route callers to the
-// appropriate handler. Flags and operands can be set up easily.
+// appropriate handler.
+//
+// There are three kinds of command line arguments that clic helps to manage:
+// Commands/Subcommands, Flags (plus related flag values), and Operands.
+// Commands/subcommands each optionally have their own flags and operands. If
+// an argument of a command does not match a subcommand, and is not a flag arg
+// (i.e. it does not start with a hyphen and is not a flag value), then it will
+// be parsed as an operand if any operands have been defined.
+//
+// Argument kinds and their placements:
+//
+//	command --flag=flag-value subcommand -f flag-value operand_a operand_b
+//
+// Custom templates and template behaviors (i.e. template function maps) can be
+// set. Custom data can be attached to instances of Clic, FlagSet, Flag,
+// OperandSet, and Operand using their Meta fields for access from custom
+// templates.
 package clic
 
 import (
