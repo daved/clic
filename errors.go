7fa1f09a23f0040b81cdb6bee3f239f50ef6e43c
daved <daved@codemodus.com>
2025-02-03 01:58:34 -0800

    Update deps, add user-friendly error func

--- errors.go
+++ errors.go
@@ -2,6 +2,7 @@ package clic
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/daved/flagset"
 	"github.com/daved/operandset"
@@ -10,12 +11,32 @@ import (
 // ErrSubCmdRequired signals that a subcommand is required and not set.
 var ErrSubCmdRequired = errors.New("subcommand is required and not set")
 
-// Cause values allow callers to easily check for potential error conditions
-// easily using a switch/case and [errors.Is].
+// Cause values are provided for documentation, and to allow callers to easily
+// detect error conditions using a switch/case and [errors.Is]. If error
+// inspection is required, use [errors.As].
 var (
 	CauseSubCmdRequired   = ErrSubCmdRequired
-	CauseFlagHydrate      = &flagset.HydrateError{}
-	CauseFlagUnrecognized = &flagset.UnrecognizedFlagError{}
-	CauseOperandHydrate   = &operandset.HydrateError{}
+	CauseFlagHydrate      = &flagset.FlagHydrateError{}
+	CauseFlagUnrecognized = &flagset.FlagUnrecognizedError{}
+	CauseOperandHydrate   = &operandset.OperandHydrateError{}
 	CauseOperandMissing   = &operandset.OperandMissingError{}
 )
+
+func UserFriendlyError(err error) error {
+	if errors.Is(err, CauseSubCmdRequired) {
+		return errors.New("A subcommand is required")
+	}
+	if fhErr := (*flagset.FlagHydrateError)(nil); errors.As(err, &fhErr) {
+		return fmt.Errorf("Bad flag value for %q (%v)", fhErr.Name, fhErr.Unwrap())
+	}
+	if fuErr := (*flagset.FlagUnrecognizedError)(nil); errors.As(err, &fuErr) {
+		return fmt.Errorf("Unrecognized flag %q", fuErr.Name)
+	}
+	if ohErr := (*operandset.OperandHydrateError)(nil); errors.As(err, &ohErr) {
+		return fmt.Errorf("Bad operand value for %q (%v)", ohErr.Name, ohErr.Unwrap())
+	}
+	if omErr := (*operandset.OperandMissingError)(nil); errors.As(err, &omErr) {
+		return fmt.Errorf("Operand %q is required", omErr.Name)
+	}
+	return err
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/daved/clic
 go 1.23.2
 
 require (
-	github.com/daved/flagset v0.0.0-20250203075035-68804c7d2399
-	github.com/daved/operandset v0.0.0-20250203074815-173efa147c1e
+	github.com/daved/flagset v0.0.0-20250203090156-c414ebacde42
+	github.com/daved/operandset v0.0.0-20250203092754-5d0780f29146
 )
